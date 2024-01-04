11ab078d65aa2fe70baef1e2cf799e82595d41e2
Brendon Gomes <brendongomes40@gmail.com>
2024-01-04 19:42:09 -0300

    add jpa annotations

--- src/main/kotlin/com/brendon/creditsystem/model/Credit.kt
+++ src/main/kotlin/com/brendon/creditsystem/model/Credit.kt
@@ -1,16 +1,32 @@
 package com.brendon.creditsystem.model
 
 import com.brendon.creditsystem.enumeration.Status
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
+@Table(name = "Credit")
 data class Credit(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long? = null,
+
+        @Column(nullable = false, unique = true)
         val creditCode: UUID = UUID.randomUUID(),
+
+        @Column(nullable = false)
         val creditValue: BigDecimal = BigDecimal.ZERO, // valor do crédito solicitaddo
+
+        @Column(nullable = false)
         val dayFirstInstallment: LocalDate, // primeiro dia da parcela
+
+        @Column(nullable = false)
         val numberOfInstalalments: Int = 0, // número de parcelas
+
+        @Enumerated
         val status: Status = Status.IN_PROGRESS,
+
+        @ManyToOne
         val customer: Customer? = null
 )
--- src/main/kotlin/com/brendon/creditsystem/model/Customer.kt
+++ src/main/kotlin/com/brendon/creditsystem/model/Customer.kt
@@ -1,12 +1,33 @@
 package com.brendon.creditsystem.model
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "Customer")
 data class Customer(
+        @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
         var id: Long? = null,
+
+        @Column(nullable = false)
         var firstName: String = "",
+
+        @Column(nullable = false)
         var lastName: String = "",
+        @Column(nullable = false, unique = true)
         val cpf: String,
-        var email: String = "",
+
+        @Column(nullable = false, unique = true)
+        email: String = "",
+        @Column(nullable = false)
         var password: String = "",
+
+        @Column(nullable = false)
+        @Embedded
         var address: Address = Address(),
+
+        @OneToMany(fetch = FetchType.LAZY,
+                cascade = arrayOf(CascadeType.REMOVE),
+                mappedBy = "customer")
+        @Column(nullable = false)
         var creditsval : List<Credit> = mutableListOf(),
 )
