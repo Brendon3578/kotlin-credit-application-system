9953be051c8ed2ebd51ed04da8380fc1e13c3c10
Brendon Gomes <brendongomes40@gmail.com>
2024-03-05 17:44:38 -0300

    ":white_check_mark: add successful customer service test and credit repository"

--- src/test/kotlin/com/brendon/creditsystem/service/CustomerServiceTest.kt
+++ src/test/kotlin/com/brendon/creditsystem/service/CustomerServiceTest.kt
@@ -9,17 +9,24 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.test.context.ActiveProfiles
 import java.math.BigDecimal
 import java.util.*
 
+//@ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
 class CustomerServiceTest {
-    @MockK lateinit var customerRepository: CustomerRepository
-    @InjectMockKs lateinit var customerService: CustomerService
+    @MockK
+    lateinit var customerRepository: CustomerRepository
+
+    @InjectMockKs
+    lateinit var customerService: CustomerService
 
     @Test
     fun `should create customer`() {
@@ -31,7 +38,7 @@ class CustomerServiceTest {
         //then
         Assertions.assertThat(actual).isNotNull
         Assertions.assertThat(actual).isSameAs(fakeCustomer)
-        verify(exactly = 1) { customerRepository.save(fakeCustomer)}
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
 
     }
 
@@ -64,6 +71,20 @@ class CustomerServiceTest {
             .isThrownBy { customerService.findById(fakeId) }
             .withMessage("Id $fakeId not found")
         verify(exactly = 1) { customerRepository.findById(fakeId) }
+    }
+
+    @Test
+    fun `should delete customer by id`() {
+        //given
+        val fakeId: Long = Random().nextLong()
+        val fakeCustomer: Customer = buildCustomer(id = fakeId)
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.delete(fakeCustomer) } just runs
+        //when
+        customerService.delete(fakeId)
+        //then
+        verify(exactly = 1) { customerRepository.findById(fakeId) }
+        verify(exactly = 1) { customerRepository.delete(fakeCustomer) }
     }
 
     private fun buildCustomer(
