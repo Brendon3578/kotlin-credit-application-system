a080657fc82cb11b3c6a724f5fc86553f6537f98
Brendon Gomes <brendongomes40@gmail.com>
2024-03-04 22:52:42 -0300

    :white_check_mark: add successful customer service test

---
+++ src/test/kotlin/com/brendon/creditsystem/service/CustomerServiceTest.kt
@@ -0,0 +1,59 @@
+package com.brendon.creditsystem.service
+
+import com.brendon.creditsystem.model.Address
+import com.brendon.creditsystem.model.Customer
+import com.brendon.creditsystem.repository.CustomerRepository
+import com.brendon.creditsystem.service.impl.CustomerService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import java.math.BigDecimal
+
+@ExtendWith(MockKExtension::class)
+class CustomerServiceTest {
+    @MockK lateinit var customerRepository: CustomerRepository
+    @InjectMockKs lateinit var customerService: CustomerService
+
+    @Test
+    fun `should create customer`() {
+        //given
+        val fakeCustomer: Customer = buildCustomer()
+        every { customerRepository.save(any()) } returns fakeCustomer
+        //when
+        val actual: Customer = customerService.save(fakeCustomer)
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.save(fakeCustomer)}
+
+    }
+
+    private fun buildCustomer(
+        firstName: String = "John",
+        lastName: String = "Doe",
+        cpf: String = "86248845867",
+        email: String = "johndoe@email.com",
+        password: String = "12345",
+        zipCode: String = "09170570",
+        street: String = "Rua Arduino Novella",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
+}
