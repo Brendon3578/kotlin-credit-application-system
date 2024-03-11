0c466c2f24edbe84cf910e13c623310dd5b8faf8
Brendon Gomes <brendongomes40@gmail.com>
2024-03-11 16:25:43 -0300

    :white_check_mark: add successful CreditRepository Test

--- src/test/kotlin/com/brendon/creditsystem/repository/CreditRepositoryTest.kt
+++ src/test/kotlin/com/brendon/creditsystem/repository/CreditRepositoryTest.kt
@@ -1,10 +1,21 @@
 package com.brendon.creditsystem.repository
 
+import com.brendon.creditsystem.dto.request.CreditDto
+import com.brendon.creditsystem.model.Address
+import com.brendon.creditsystem.model.Credit
+import com.brendon.creditsystem.model.Customer
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.time.LocalDate
+import java.time.Month
+import java.util.*
 
 @ActiveProfiles("test")
 @DataJpaTest
@@ -14,7 +25,71 @@ import org.springframework.test.context.ActiveProfiles
 )
 class CreditRepositoryTest {
     @Autowired
-    lateinit var customerRepository: CustomerRepository
+    lateinit var creditRepository: CreditRepository
     @Autowired
     lateinit var testEntityManager: TestEntityManager
+
+    private lateinit var customer: Customer
+    private lateinit var credit1: Credit
+    private lateinit var credit2: Credit
+
+    @BeforeEach fun setup() {
+        customer = testEntityManager.persist(buildCustomer())
+        credit1 = testEntityManager.persist(buildCredit(customer = customer))
+        credit2 = testEntityManager.persist(buildCredit(customer = customer))
+    }
+
+    @Test
+    fun `should find credit by credit code`() {
+        //given
+        val creditCode1 = UUID.fromString("aa547c0f-9a6a-451f-8c89-afddce916a29")
+        val creditCode2 = UUID.fromString("49f740be-46a7-449b-84e7-ff5b7986d7ef")
+        credit1.creditCode = creditCode1
+        credit2.creditCode = creditCode2
+
+        // when
+        val fakeCredit1: Credit = creditRepository.findByCreditCode(creditCode1)!!
+        val fakeCredit2: Credit = creditRepository.findByCreditCode(creditCode2)!!
+
+        // then
+        Assertions.assertThat(fakeCredit1).isNotNull
+        Assertions.assertThat(fakeCredit2).isNotNull
+        Assertions.assertThat(fakeCredit1).isSameAs(credit1)
+        Assertions.assertThat(fakeCredit2).isSameAs(credit2)
+    }
+
+    private fun buildCredit(
+        creditValue: BigDecimal = BigDecimal.valueOf(500.0),
+        dayFirstInstallment: LocalDate = LocalDate.of(2024, Month.MARCH, 11),
+        numberOfInstallments: Int = 5,
+        customer: Customer
+    ): Credit = Credit (
+        creditValue = creditValue,
+        dayFirstInstallment = dayFirstInstallment,
+        numberOfInstallments = numberOfInstallments,
+        customer = customer
+    )
+
+    private fun buildCustomer(
+        firstName: String = "John",
+        lastName: String = "Doe",
+        cpf: String = "86248845867",
+        email: String = "johndoe@email.com",
+        password: String = "12345",
+        zipCode: String = "09170570",
+        street: String = "Rua Arduino Novella",
+        income: BigDecimal = BigDecimal.valueOf(1000.0)
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+    )
+
 }
